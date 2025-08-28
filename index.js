ce7b4dad9b4096adda4e9d7e5efd7c760c6d824c
Rajkumar Yogi <rajkumar6777y@gmail.com>
2025-08-28 12:27:05 +0530

    cors permission added with cors option

--- index.js
+++ index.js
@@ -3,13 +3,27 @@ import express from "express";
 import cors from "cors";
 import summaryRoutes from "./routes/emailRoute.js";
 
+// List of websites you want to allow
+const allowedOrigins = [
+  'https://zentrok.vercel.app', 
+  'https://www.zentrok.com',
+  'http://localhost:3000' // Good to include for local development
+];
+
 const app = express();
-app.use(cors({
-  origin: "https://www.zentrok.com" || "http://localhost:3000" || "https://zentrok.vercel.app",
-  methods: ["GET", "POST"],
-  credentials: true
-}
-));
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+// Use the CORS options
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/api/subscribe", summaryRoutes);
